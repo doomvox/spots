0b45f9c7147ae5a5a1877276d3eb1b210a5adac9
Joseph Brenner <doomvox@gmail.com>
2019-04-16 18:10:20 -0700

    Automatic git commit by perlnow: Adding Spots::Rectangle::TestData to milla cpan-style project

---
+++ lib/Spots/Rectangle/TestData.pm
@@ -0,0 +1,90 @@
+package Spots::Rectangle::TestData;
+use Moo;
+use MooX::Types::MooseLike::Base qw(:all);
+
+=head1 NAME
+
+Spots::Rectangle::TestData - The great new Spots::Rectangle::TestData! TODO revise this
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+# TODO revise these before shipping
+our $VERSION = '0.01';
+my $DEBUG = 1;
+
+=head1 SYNOPSIS
+
+   use Spots::Rectangle::TestData;
+   my $obj = Spots::Rectangle::TestData->new({ ...  });
+
+   # TODO expand on this
+
+=head1 DESCRIPTION
+
+Spots::Rectangle::TestData is a module that ...
+
+TODO expand this stub documentation, which was created by perlnow.el.
+
+=head1 METHODS
+
+=over
+
+=cut
+
+use 5.10.0;
+use Carp;
+use Data::Dumper;
+
+=item new
+
+Creates a new Spots::Rectangle::TestData object.
+
+Takes a hashref as an argument, with named fields identical
+to the names of the object attributes. These attributes are:
+
+=over
+
+=item <TODO fill-in attributes here... most likely, sort in order of utility>
+
+=back
+
+=cut
+
+# Example attribute:
+# has is_loop => ( is => 'rw', isa => Int, default => 0 );
+# Tempted to use Mouse over Moo so I can do my usual "isa => 'Int'"
+
+{ no warnings 'once'; $DB::single = 1; }
+
+### Fill in additional methods here
+### hint: perlnow-insert-method
+
+
+
+
+=back
+
+=head1 AUTHOR
+
+Joseph Brenner, E<lt>doom@kzsu.stanford.eduE<gt>,
+16 Apr 2019
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2019 by Joseph Brenner
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+No warranty is provided with this code.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1;
