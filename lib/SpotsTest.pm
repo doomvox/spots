62e590b7a9ef0d1715dba00f1fb423a173b593c9
Joseph Brenner <doom@fandango.obsidianrook.com>
2019-05-28 21:56:24 -0700

    Automatic git commit by perlnow: Adding SpotsTest to milla cpan-style project

---
+++ lib/SpotsTest.pm
@@ -0,0 +1,99 @@
+package SpotsTest;
+use Moo;
+use MooX::Types::MooseLike::Base qw(:all);
+
+=head1 NAME
+
+SpotsTest - The great new SpotsTest! TODO revise this
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+# TODO revise these before shipping
+our $VERSION = '0.01';
+my $DEBUG = 1;
+
+=head1 SYNOPSIS
+
+   use SpotsTest;
+   my $obj = SpotsTest->new({ ...  });
+
+   # TODO expand on this
+
+=head1 DESCRIPTION
+
+SpotsTest is a module that ...
+
+TODO expand this stub documentation, which was created by perlnow.el.
+
+=head1 METHODS
+
+=over
+
+=cut
+
+use 5.10.0;
+use Carp;
+use Data::Dumper;
+use File::Path      qw( mkpath );
+use File::Basename  qw( fileparse basename dirname );
+use File::Copy      qw( copy move );
+use autodie         qw( :all mkpath copy move ); # system/exec along with open, close, etc
+use Cwd             qw( cwd abs_path );
+use Env             qw( HOME );
+use List::Util      qw( first max maxstr min minstr reduce shuffle sum any );
+use List::MoreUtils qw( zip uniq );
+use String::ShellQuote qw( shell_quote );
+
+
+=item new
+
+Creates a new SpotsTest object.
+
+Takes a hashref as an argument, with named fields identical
+to the names of the object attributes. These attributes are:
+
+=over
+
+=item <TODO fill-in attributes here... most likely, sort in order of utility>
+
+=back
+
+=cut
+
+# Example attribute:
+# has is_loop => ( is => 'rw', isa => Int, default => 0 );
+
+{ no warnings 'once'; $DB::single = 1; }
+
+### Fill in additional methods here
+### hint: perlnow-insert-method
+
+
+
+
+=back
+
+=head1 AUTHOR
+
+Joseph Brenner, E<lt>doom@kzsu.stanford.eduE<gt>,
+28 May 2019
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2019 by Joseph Brenner
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+No warranty is provided with this code.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1;
